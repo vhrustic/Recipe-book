7a19bdcc92b8e4399e1038079ffe81e3036b8e49
Vejsil <bakerundo@hotmail.com>
2016-03-20 15:23:58 +0100

    Template level subs, flowRouter with parameters

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,2 +1,12 @@
-Meteor.subscribe('recipes');
-console.log(Meteor.settings.public.ga.account);
+Template.Recipes.onCreated(function () {
+	 var self = this;
+	 self.autorun(function () {
+	 	 self.subscribe('recipes'); 
+	 });
+});
+
+Template.Recipes.helpers({
+	recipes: ()=> {
+		return Recipes.find({});
+	}
+});
